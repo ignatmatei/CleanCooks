65a48c3c6179b5f52cc7632e2767ac5a4f80d82f
ignatmatei <mateiignat03@gmail.com>
2024-01-29 20:04:04 +0200

    update frontend script

--- src/fe/CleanCooks/frontend_deploy_script.ps1
+++ src/fe/CleanCooks/frontend_deploy_script.ps1
@@ -1,6 +1,6 @@
 ng build --base-href /CleanCooks/
-$source = "C:\Users\matei\Documents\CleanCooks\src\fe\CleanCooks\dist\clean-cooks\browser"
-$destination = "C:Users\matei\Documents\CleanCooks\docs"
+$source = "C:.\dist\clean-cooks\browser"
+$destination = "..\..\..\docs"
 Copy-Item $source $destination -Recurse
 Write-Host "Done!"
 
