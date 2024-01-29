0769ee8867ca6a9af227ba3da07d087254600e16
ignatmatei <mateiignat03@gmail.com>
2024-01-29 10:10:30 +0200

    Update UserController.java

--- src/be/src/main/java/com/example/ccooks/controllers/UserController.java
+++ src/be/src/main/java/com/example/ccooks/controllers/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://ignatmatei.github.io/"})
 public class UserController {
     private final UserService userService;
     @GetMapping("/boris")
